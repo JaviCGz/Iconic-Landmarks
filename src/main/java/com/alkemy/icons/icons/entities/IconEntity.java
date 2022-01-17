b48a6cde54fd842eb3a3ff1cf447f91e300d476c
Javi Cáceres Giménez <javi.caceresgimenez@gmail.com>
2022-01-17 18:27:42 -0300

    Added entities

---
+++ src/main/java/com/alkemy/icons/icons/entities/IconEntity.java
@@ -0,0 +1,43 @@
+package com.alkemy.icons.icons.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "icon")
+@Getter
+@Setter
+/*@SQLDelete(sql = "UPDATE icon SET deleted = true WHERE id=?")
+@Where(clause = "deleted = false")*/
+public class IconEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+    private String image;
+    @Column(name = "icon_name")
+    private String iconName;
+    @Column(name = "creation_date")
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate creationDate;
+    private long height;
+    private String history;
+    /*private boolean deleted = Boolean.false;*/
+    @ManyToMany(mappedBy = "icons", cascade = CascadeType.ALL)
+    private List<CountryEntity> countries = new ArrayList<>();
+
+/*    //Add and delete countries
+    public void addCountry(CountryEntity country) {
+        this.countries.add(country);
+    }
+    public void removeCountry(CountryEntity country) {
+        this.countries.remove(country);
+    }*/
+}
---
+++ src/main/java/com/alkemy/icons/icons/entity/ContinentEntity.java
@@ -0,0 +1,17 @@
+package com.alkemy.icons.icons.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "continent")
+@Getter
+@Setter
+public class ContinentEntity {
+
+}
